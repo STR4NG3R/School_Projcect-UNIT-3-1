304407e2ea0076110c5a460ed9835ea7cb8ad9c7
Pablo Eduardo <STR4NG3R@localhost.localdomain>
2017-04-21 15:57:44 -0500

    Añadi get y set para las fechas, cambiando los valores por default de los precios

--- src/examenenproyecto/Metodos.java
+++ src/examenenproyecto/Metodos.java
@@ -4,41 +4,45 @@ import java.util.Arrays;
 import javax.swing.*;
 public class Metodos {
     int conta=0;
-    Datos a1[]=new Datos[100000];
-        String mensa = "";
+    Datos a1[]=new Datos[1000];
+    Fecha syste=new Fecha();
       public void seleccion(){
-          int opcion;
-           do{
-          opcion=Integer.parseInt(JOptionPane.showInputDialog(null,"----------Eventos Solis-----------\n1.-Regitrar Servicio.\n2.-Ver servicios\n3.-Eliminar servicios\n4.-Salir"));
-          switch(opcion){
+          String opcion;
+          int valida;
+           do{ 
+               do{
+          opcion=JOptionPane.showInputDialog(null,"----------Eventos Solis-----------\n1.-Registrar Servicio.\n2.-Ver servicios\n3.-Eliminar servicios\n4.-Salir");
+          if(!"".equals(opcion));{
+          valida=Integer.parseInt(opcion);
+               }
+               }while("".equals(opcion)||!(valida>0&&valida<5));
+          switch(valida){
               case 1:
                a1[conta]=Registrar();
                conta++;
                break;
-               case 2:{
+               case 2:
+                   if (conta==0){
+                   JOptionPane.showMessageDialog(null,"No tiene absolutamente nada registrado");
+                   break;}
                    verRegistrado();
-               break;}
+               break;
                case 3:{
-                   
-               break;}
-               case 4:break;
-               default:{
-                   JOptionPane.showMessageDialog(null,"Haga las cosas bien por favor");
+                 consultar();  
                break;}
           }    
-          }while (opcion!=4);
+          }while (valida==4);
           JOptionPane.showMessageDialog(null,"Hasta luego");
       }
-      
  public Datos Registrar(){
      Datos a=new Datos();
         double precioSalon[]=new double[]{4000,10000,30000};
         String salones[]=new String[] {"Economico","Estandar","Deluxe"};
         ArrayList<String> salon = new ArrayList<>(Arrays.asList(salones));
-        Object sa = JOptionPane.showInputDialog(null, "Salones","Salón", JOptionPane.QUESTION_MESSAGE, null,salones,"");
+        Object sa = JOptionPane.showInputDialog(null,"Salones","Salón",JOptionPane.QUESTION_MESSAGE,null,salones,"");
         int cualsalon = salon.indexOf(sa.toString());
         a.setPreciosalon(precioSalon[cualsalon]);
-        fechaEntra();
+        a.fechaEntra();
      int personasAsistiran;
      String sn;
      a.setNopersonas(personasAsistiran=Integer.parseInt( JOptionPane.showInputDialog(null, "Ingrese el numero de personas que se tienen programadas para el evento")));
@@ -51,7 +55,6 @@ public class Metodos {
         Object me = JOptionPane.showInputDialog(null, "Platillos", "Platillo Fuerte", JOptionPane.QUESTION_MESSAGE, null,platillos, "");
         int cualPlato = pal.indexOf(me.toString());
         a.setPreciocomida(precioplatos[cualPlato]);
-         System.out.println(cualPlato);
      }
       sn=(String) JOptionPane.showInputDialog(null, "Postres", "¿Habra postre en su evento?", JOptionPane.QUESTION_MESSAGE, null,son, "");
        if(sn.equals("Si")){
@@ -61,43 +64,68 @@ public class Metodos {
         Object me = JOptionPane.showInputDialog(null, "Postres","Postres", JOptionPane.QUESTION_MESSAGE, null,platillos, "");
         int cualPlato = pal.indexOf(me.toString());
         a.setPreciocomida(precioplatos[cualPlato]);
-        System.out.println(cualPlato);
        }
         sn=(String) JOptionPane.showInputDialog(null, "Bebidas", "¿Habra bebidas en su  evento?", JOptionPane.QUESTION_MESSAGE, null,son, "");
         if(sn.equals("Si")){
-        double precioplatos[]=new double[]{20,20,20,350.5,400,85.75,60};
-        String bebidas[]=new String[] {"Coca-Cola","Miranda","Fresca","Tequila","Whiskey","Sidra","Personalizado"};
+        double precioplatos[]=new double[]{10,10,8,25.5,23,25.75,25};
+        String bebidas[]=new String[] {"Coca-Cola","Mirinda","Fresca","Tequila","Whiskey","Sidra","Personalizado"};
         ArrayList<String> bebid = new ArrayList<>(Arrays.asList(bebidas));
         Object me = JOptionPane.showInputDialog(null, "Bebidas", "Bebida", JOptionPane.QUESTION_MESSAGE, null,bebidas,"");
         int cualPlato = bebid.indexOf(me.toString());
-        a.setPrecioBebida(precioplatos[cualPlato]);
-        System.out.println(cualPlato);
-       }
+        a.setPreciobebida(precioplatos[cualPlato]);
+        }
          sn=(String) JOptionPane.showInputDialog(null, "Música", "¿Quiere el servicio de musica que le ofrecemos?", JOptionPane.QUESTION_MESSAGE, null,son, "");
         if(sn.equals("Si")){
         a.setPreciomusica(500);
         }
-        System.out.println(año());
+        do{
+        int dia,mes,año;
+        a.setAño(año=Integer.parseInt(JOptionPane.showInputDialog("Introducir el año que se realizara el evnto")));
+        a.setDia(dia=Integer.parseInt(JOptionPane.showInputDialog("Introducir el dia que se realizara el evento")));
+        a.setMes(mes=Integer.parseInt(JOptionPane.showInputDialog("Introducir el mes en el que se hara el evento")));
+        a.fechaCorrecta();
+            System.out.println(a.fecha2());
+        }while((a.fechaCorrecta())==false);
         a.setAño(año());
         a.generarId();
+        a.gTotal();
 return a; 
  }
 void verRegistrado(){
-for (int i=0;i<conta;i++){
-    System.out.println( a1[i].getNopersonas());  
-    mensa=a1[i].generarId()+"\n";
+    String mensa = "";
+    String cabecera=String.format("%s%n%s%44s%15s%30s%n%s%n",
+            "-----------------------------------------------------------------------------------------------------------",
+            "|ID","|FECHA DE REGISTRO","|PRECIO","|DIA DEL CONVIVIO|",
+            "-----------------------------------------------------------------------------------------------------------"); 
+    int c=cabecera.length();
+    System.out.println(c);
+for (int i=0;i<conta;i++){  
+    if (!(a1[i]==null)){
+    mensa=mensa+String.format(" %s%30s%22s%19s%n",a1[i].getID(),a1[i].getFechaEntra(),a1[i].getTotal(),a1[i].fecha2());
+    }
 }
-JOptionPane.showMessageDialog(null, mensa);
+JOptionPane.showMessageDialog(null,cabecera+mensa);
 } 
- String fechaEntra(){
-     Fecha syste=new Fecha();
-     syste.asignarFecha();
-   String fecha=syste.getDia()+"/"+syste.getMes()+"/"+syste.getAño()+"."+syste.getHora()+"."+syste.getMinute()+"."+syste.getSeg();       
- return fecha; 
- }
+
  int año(){
-    Fecha syste=new Fecha();
     syste.asignarFecha();
     return syste.getAño();
  }
+ void consultar(){
+    String idn="";
+    boolean a=true;
+    idn=JOptionPane.showInputDialog("Ingrese el ID para eliminar el evento");
+    for (int i=0;i<conta;i++){
+        if (a1[i].getID().equals(idn)){   
+        JOptionPane.showMessageDialog(null,"Se ha elimiado elvento: "+a1[i].getID()+". Que es el numero: "+i);
+        a1[i]=null;
+        i=conta;
+        a=false;
+        break;
+        }
+        if (a==true){
+        JOptionPane.showMessageDialog(null,"No hay ningun ID así. intentelo de nuevo.");    
+        break;}
+        }    
+}
 }
